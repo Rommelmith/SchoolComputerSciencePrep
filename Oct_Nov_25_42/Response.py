8a1aa59e0da9ccf3891614d5cbcaa5d251de4a53
Rommelmith <rommelabbas59@gmail.com>
2026-04-20 09:17:22 +0500

    Paper 2025 oct_Nov_42 Done

--- Oct_Nov_25_42/Response.py
+++ Oct_Nov_25_42/Response.py
@@ -48,77 +48,129 @@
 #     Bird2.Move(direction, time)
 
 ###################### Question 2 #######################
-import random
+# import random
+#
+# arr = [None]*20
+# count = 0
+# while count<20:
+#     num = random.randint(0, 100)
+#     if num in arr:
+#         print("")
+#     else:
+#         arr[count] = num
+#         count += 1
+#
+# def PrintArray(Myarr):
+#     line = ""
+#     for i in range(len(Myarr)):
+#         line = line + str(Myarr[i]) + " "
+#     print(line)
+#
+#
+# def BubbleSort(Myarr):
+#     n = len(Myarr)
+#     for i in range(n):
+#         swapped = False
+#         for j in range(0, n-1-i):
+#             if Myarr[j] > Myarr[j + 1]:
+#                 Myarr[j], Myarr[j + 1] = Myarr[j + 1], Myarr[j]
+#                 swapped = True
+#         if not swapped:
+#                 break
+#     return Myarr
+# # PrintArray(arr)
+# BubbleSort(arr)
+# # print("Sorted")
+# # PrintArray(arr)
+#
+# # Part e forward
+#
+# ######################## Question 3 #################################
+#
+# TreeArray = [
+#     [],# pointer
+#     [] # data
+# ]
+# RootPointer = -1
+# FreeNode = 0
+#
+# def RecursiveBinarySearch(Myarr, lowerBound, UpperBound, item):
+#     if UpperBound >= lowerBound:
+#         # ArrLen = len(Myarr)
+#         mid = int((UpperBound+lowerBound)/2)
+#         if Myarr[mid] == item:
+#             return mid
+#         else:
+#             if Myarr[mid] > item:
+#                 # UpperBound =
+#                 return RecursiveBinarySearch(Myarr, lowerBound, mid-1, item)
+#             else:
+#                 # lowerBound =
+#                 return RecursiveBinarySearch(Myarr, mid+1, UpperBound, item)
+#
+#
+#     else:
+#         return -1
+#
+# arr1 = [1,2,3,4,5,6,7,88,90,91]
+# print(RecursiveBinarySearch(arr1, 0, len(arr1), 1))
 
-arr = [None]*20
-count = 0
-while count<20:
-    num = random.randint(0, 100)
-    if num in arr:
-        print("")
-    else:
-        arr[count] = num
-        count += 1
+######################## Question 3 ###############################
 
-def PrintArray(Myarr):
-    line = ""
-    for i in range(len(Myarr)):
-        line = line + str(Myarr[i]) + " "
-    print(line)
+TreeArray = [[-1,-1,-1] for i in range(50)]
 
-
-def BubbleSort(Myarr):
-    n = len(Myarr)
-    for i in range(n):
-        swapped = False
-        for j in range(0, n-1-i):
-            if Myarr[j] > Myarr[j + 1]:
-                Myarr[j], Myarr[j + 1] = Myarr[j + 1], Myarr[j]
-                swapped = True
-        if not swapped:
-                break
-    return Myarr
-# PrintArray(arr)
-BubbleSort(arr)
-# print("Sorted")
-# PrintArray(arr)
-
-# Part e forward
-
-######################## Question 3 #################################
-
-TreeArray = [
-    [],# pointer
-    [] # data
-]
 RootPointer = -1
 FreeNode = 0
 
-def RecursiveBinarySearch(Myarr, lowerBound, UpperBound, item):
-    if UpperBound >= lowerBound:
-        # ArrLen = len(Myarr)
-        mid = int((UpperBound+lowerBound)/2)
-        if Myarr[mid] == item:
-            return mid
-        else:
-            if Myarr[mid] > item:
-                # UpperBound =
-                return RecursiveBinarySearch(Myarr, lowerBound, mid-1, item)
-            else:
-                # lowerBound =
-                return RecursiveBinarySearch(Myarr, mid+1, UpperBound, item)
 
-
+def AddNode(Mynum:int):
+    global RootPointer, FreeNode, TreeArray
+    if FreeNode >= 50:
+        print("The tree is full")
     else:
-        return -1
+        if RootPointer == -1:
+            RootPointer = FreeNode
+            TreeArray[FreeNode][1] = Mynum
+            FreeNode += 1
+        else:
+            i =RootPointer
+            while True:
+                if Mynum < TreeArray[i][1]:
+                    if TreeArray[i][0] == -1:
+                        TreeArray[FreeNode][1] = Mynum
+                        TreeArray[i][0] = FreeNode
+                        FreeNode += 1
+                        break
+                    else:
+                        i = TreeArray[i][0]
+                else:
+                    if TreeArray[i][2] == -1:
+                        TreeArray[FreeNode][1] = Mynum
+                        TreeArray[i][2] = FreeNode
+                        FreeNode += 1
+                        break
+                    else:
+                        i = TreeArray[i][2]
 
-arr1 = [1,2,3,4,5,6,7,88,90,91]
-print(RecursiveBinarySearch(arr1, 0, len(arr1), 1))
 
 
+with open("TreeData.txt", "r") as f:
+    for line in f:
+        line = line.strip()
+        num = int(line)
+        AddNode(num)
 
+def WriteAllToFile():
+    global TreeArray
+    try:
+        with open("Tree.txt", "w") as f:
+            i = 0
+            while i < 50:
+                line = TreeArray[i][1]
+                f.write(f"{TreeArray[i][0]},{str(line)},{TreeArray[i][2]}\n")
+                i += 1
+    except Exception as e:
+        print(e)
 
-
-
-
+WriteAllToFile()
 
