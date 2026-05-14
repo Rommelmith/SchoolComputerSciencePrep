157c1c6336ad514d0ad874c5a48bb3f09430fcec
Rommelmith <rommelabbas59@gmail.com>
2026-05-15 01:51:34 +0500

    May June 25 43

--- LinkedListWithClass.py
+++ LinkedListWithClass.py
@@ -1,80 +1,49 @@
-# class Node:
-#     def __init__(self, TheData:int):
-#         self.__TheData = TheData
-#         self.__NextNode = None
-#
-#     def GetData(self):
-#         return self.__TheData
-#     def GetNextNode(self):
-#         return self.__NextNode
-#
-#     def SetNextNode(self, NewNode):
-#         self.__NextNode = NewNode
-#
-# class LinkedList:
-#     def __init__(self):
-#         self.__HeadNode = None
-#
-#     def InsertNode(self, data:int):
-#         NewNode = Node(data)
-#         NewNode.SetNextNode(self.__HeadNode)
-#         self.__HeadNode = NewNode
-#
-#     def Traverse(self):
-#         result = ""
-#         current = self.__HeadNode
-#         while current is not None:
-#             result = result + str(current.GetData()) + " "
-#             current = current.GetNextNode()
-#         return result
-#
-#     def RemoveData(self, IntRemove:int):
-#         removed = False
-#         if self.__HeadNode == None:
-#             return removed
-#         else:
-#             if self.__HeadNode.GetData() == IntRemove:
-#                 nextNode = self.__HeadNode.GetNextNode()
-#                 self.__HeadNode = nextNode
-#                 removed = True
-#                 return removed
-#         currentNode = self.__HeadNode
-#         while currentNode.GetNextNode() is not None:
-#             if currentNode.GetNextNode().GetData() == IntRemove:
-#                 currentNode.SetNextNode(currentNode.GetNextNode().GetNextNode())
-#                 removed = True
-#                 return removed
-#             currentNode = currentNode.GetNextNode()
-#         return False
-
-
-
 class Node:
-    def __init__(self, TheData):
+    def __init__(self, TheData:int):
         self.__TheData = TheData
         self.__NextNode = None
-
 
     def GetData(self):
         return self.__TheData
     def GetNextNode(self):
         return self.__NextNode
+
     def SetNextNode(self, NewNode):
         self.__NextNode = NewNode
 
-
 class LinkedList:
+    def __init__(self):
+        self.__HeadNode = None
 
+    def InsertNode(self, data:int):
+        NewNode = Node(data)
+        NewNode.SetNextNode(self.__HeadNode)
+        self.__HeadNode = NewNode
 
+    def Traverse(self):
+        result = ""
+        current = self.__HeadNode
+        while current is not None:
+            result = result + str(current.GetData()) + " "
+            current = current.GetNextNode()
+        return result
 
-
-
-
-
-
-
-
-
-
-
+    def RemoveData(self, IntRemove:int):
+        removed = False
+        if self.__HeadNode == None:
+            return removed
+        else:
+            if self.__HeadNode.GetData() == IntRemove:
+                nextNode = self.__HeadNode.GetNextNode()
+                self.__HeadNode = nextNode
+                removed = True
+                return removed
+        currentNode = self.__HeadNode
+        while currentNode.GetNextNode() is not None:
+            if currentNode.GetNextNode().GetData() == IntRemove:
+                currentNode.SetNextNode(currentNode.GetNextNode().GetNextNode())
+                removed = True
+                return removed
+            currentNode = currentNode.GetNextNode()
+        return False
 
--- May_June_25_42/Response.py
+++ May_June_25_42/Response.py
@@ -1,121 +1,213 @@
-# ################### Question 1 #####################
+# # ################### Question 1 #####################
+#
+# TopOfStack = -1
+# Stack = ["-1"]*20
+#
+# def Push(item:str):
+#     global TopOfStack, Stack
+#     if TopOfStack == 19:
+#         return -1
+#
+#     else:
+#         TopOfStack += 1
+#         Stack[TopOfStack] = item
+#         return 1
+#
+# def Pop():
+#     global TopOfStack, Stack
+#     if TopOfStack == -1:
+#         return "-1"
+#
+#     else:
+#         item = Stack[TopOfStack]
+#         TopOfStack -= 1
+#         return item
+#
+#
+# def ReadData(filename):
+#     global TopOfStack, Stack
+#     try:
+#         with open(filename, "r") as file:
+#             for line in file:
+#                 pushStatus = Push(line.strip())
+#                 if pushStatus == -1:
+#                     print ("Stack full")
+#     except Exception as e:
+#         print(f"Error reading the file: {e}")
+#
+#
+# def Calculate():
+#     global TopOfStack, Stack
+#     total = Pop()
+#     total = float(total)
+#
+#
+#     while TopOfStack != -1:
+#         op = Pop()
+#         num = float(Pop())
+#
+#         if op == "+":
+#             total = total + num
+#         elif op == "-":
+#             total = total - num
+#         elif op == "*":
+#             total = total * num
+#         elif op == "/":
+#             total = total / num
+#         elif op == "^":
+#             total = total ** num
+#
+#     return total
+#
+# filename = input("Enter the file name: ")
+# ReadData(filename)
+# calculatedNum = Calculate()
+# print(calculatedNum)
+#
+# #################### Question 2 #####################
+#
+# HashTable = [None]*200
+# Spare = [None]*100
+# FreeSpace = 0
+#
+# class Record:
+#     def __init__(self, key:int, Item1:int, Item2:int):
+#         self.key = key
+#         self.Item1 = Item1
+#         self.Item2 = Item2
+#
+# def initialize():
+#     global HashTable, Spare, FreeSpace
+#     for i in range(200):
+#         HashTable[i] = Record(-1, -1, -1)
+#     for d in range(100):
+#         Spare[d] = Record(-1, -1, -1)
+#     FreeSpace = 0
+#
+# def CalculateHash(KeyField):
+#     keyVal = KeyField % 200
+#     return keyVal
+#
+# def InsertIntoHash(NewRecord:Record):
+#     global HashTable, Spare, FreeSpace
+#     key = NewRecord.key
+#     place = CalculateHash(NewRecord.key)
+#     if HashTable[place].key != -1:
+#         Spare[FreeSpace] = NewRecord
+#         FreeSpace += 1
+#     else:
+#         HashTable[place] = NewRecord
+#
+#
+# def CreateHashTable():
+#     global HashTable, Spare, FreeSpace
+#     with open(r"C:\Users\romme\PycharmProjects\SchoolComputerSciencePrep\May_June_25_42\HashData.txt", "r") as f:
+#         for line in f:
+#             key = int(line.split(",")[0])
+#             item1 = int(line.split(",")[1])
+#             item2 = int(line.split(",")[2])
+#             new_record = Record(key, item1, item2)
+#             InsertIntoHash(new_record)
+#
+# def PrintSpare():
+#     global Spare, FreeSpace
+#     for i in Spare:
+#         if i.key != -1:
+#             print(i.key)
+#
+# if __name__ == '__main__':
+#     initialize()
+#     CreateHashTable()
+#     PrintSpare()
 
-TopOfStack = -1
-Stack = ["-1"]*20
 
-def Push(item:str):
-    global TopOfStack, Stack
-    if TopOfStack == 19:
-        return -1
+################# PAPER 43 ##############################
+ ################## Question 1 ###############
+# def ReadData():
+#     ArrItem = [None for i in range(45)]
+#     count = 0
+#
+#     with open("Data.txt", "r") as f:
+#         for line in f.readlines():
+#             ArrItem[count] = line.strip()
+#             count += 1
+#     return ArrItem
+#
+#
+# def FormatArray(MyArr):
+#     text = ""
+#     for i in range(len(MyArr)):
+#         item = str(MyArr[i])
+#         item = item.strip()
+#         text += f"{item} "
+#     return text
+#
+# arr = ReadData()
+# print(FormatArray(arr))
+#
+#
+# def ComparrStrings(test1, test2):
+#     for i in range(len(test1)):
+#         if test1[i] < test2[i]:
+#             return 1
+#         else:
+#             return 2
+#
+# def Bubble(arr):
+#     for i in range(len(arr)):
+#         for j in range(len(arr)-1-i):
+#             if ComparrStrings(arr[j + 1], arr[j]) == 1:
+#                 arr[j], arr[j+1] = arr[j+1], arr[j]
+#
+#     return arr
 
-    else:
-        TopOfStack += 1
-        Stack[TopOfStack] = item
-        return 1
+################## Question 2 ###############
+class Horse:
+    def __init__(self, Name, MaxFenceHeight, PercentageSuccess):
+        self.__Name = Name
+        self.__MaxFenceHeight = MaxFenceHeight
+        self.__PercentageSuccess = PercentageSuccess
 
-def Pop():
-    global TopOfStack, Stack
-    if TopOfStack == -1:
-        return "-1"
+    def GetName(self):
+        return self.__Name
 
-    else:
-        item = Stack[TopOfStack]
-        TopOfStack -= 1
-        return item
-
-
-def ReadData(filename):
-    global TopOfStack, Stack
-    try:
-        with open(filename, "r") as file:
-            for line in file:
-                pushStatus = Push(line.strip())
-                if pushStatus == -1:
-                    print ("Stack full")
-    except Exception as e:
-        print(f"Error reading the file: {e}")
-
-
-def Calculate():
-    global TopOfStack, Stack
-    total = Pop()
-    total = float(total)
-
-
-    while TopOfStack != -1:
-        op = Pop()
-        num = float(Pop())
-
-        if op == "+":
-            total = total + num
-        elif op == "-":
-            total = total - num
-        elif op == "*":
-            total = total * num
-        elif op == "/":
-            total = total / num
-        elif op == "^":
-            total = total ** num
-
-    return total
-
-filename = input("Enter the file name: ")
-ReadData(filename)
-calculatedNum = Calculate()
-print(calculatedNum)
-
-#################### Question 2 #####################
-
-HashTable = [None]*200
-Spare = [None]*100
-FreeSpace = 0
-
-class Record:
-    def __init__(self, key:int, Item1:int, Item2:int):
-        self.key = key
-        self.Item1 = Item1
-        self.Item2 = Item2
-
-def initialize():
-    global HashTable, Spare, FreeSpace
-    for i in range(200):
-        HashTable[i] = Record(-1, -1, -1)
-    for d in range(100):
-        Spare[d] = Record(-1, -1, -1)
-    FreeSpace = 0
-
-def CalculateHash(KeyField):
-    keyVal = KeyField % 200
-    return keyVal
-
-def InsertIntoHash(NewRecord:Record):
-    global HashTable, Spare, FreeSpace
-    key = NewRecord.key
-    place = CalculateHash(NewRecord.key)
-    if HashTable[place].key != -1:
-        Spare[FreeSpace] = NewRecord
-        FreeSpace += 1
-    else:
-        HashTable[place] = NewRecord
+    def GetMaxFenceHeight(self):
+        return self.__MaxFenceHeight
 
 
-def CreateHashTable():
-    global HashTable, Spare, FreeSpace
-    with open(r"C:\Users\romme\PycharmProjects\SchoolComputerSciencePrep\May_June_25_42\HashData.txt", "r") as f:
-        for line in f:
-            key = int(line.split(",")[0])
-            item1 = int(line.split(",")[1])
-            item2 = int(line.split(",")[2])
-            new_record = Record(key, item1, item2)
-            InsertIntoHash(new_record)
+h1 = Horse("Beauty", 150, 72)
+h2 = Horse("Jet", 160, 65)
 
-def PrintSpare():
-    global Spare, FreeSpace
-    for i in Spare:
-        if i.key != -1:
-            print(i.key)
+Horses = [h1, h2]
+print(Horses[0].GetName())
+print(Horses[1].GetName())
 
-if __name__ == '__main__':
-    initialize()
-    CreateHashTable()
-    PrintSpare()
+class Fence:
+    def __init__(self, Height, Risk):
+        self.__Height = Height # height in Integer
+        self.__Risk = Risk # Risk in Integer
+
+    def GetHeight(self):
+        return self.__Height
+    def GetRisk(self):
+        return self.__Risk
+
+
+Course = [None for i in range(4)]
+
+for i in range(4):
+    fence = int(input("Enter Fence Height : "))
+
+    while fence>180 and fence<70:
+        fence = int(input("Please enter number between 70 to 180 : "))
+
+    risk = int(input("Enter Risk between 1 to 5 : "))
+    while risk<1 and risk>5:
+        risk = int(input("Please enter Risk between 1 to 5 : "))
+
+    name = Fence(fence, risk)
+    Course[i] = name
+print(Course)
+
+
+
